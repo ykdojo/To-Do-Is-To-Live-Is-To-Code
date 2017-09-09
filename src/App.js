00c4cd388669f33bb2de997c8f5045ac9c54c2b9
yksugi <yksu4p@gmail.com>
2017-09-09 14:23:37 -0700

    Fixed indentation

--- src/App.js
+++ src/App.js
@@ -2,37 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-	constructor() {
-    	super();
-	    this.state = {
-	      items: [{content: 'TODO Item 1'}, {id: 1, content: 'TODO Item 2'}],
-	    };
-	}
+  constructor() {
+    super();
+    this.state = {
+      items: [{content: 'TODO Item 1'}, {id: 1, content: 'TODO Item 2'}],
+    };
+}
 
-	render() {
-		const itemsToShow = this.state.items.map((item) =>
-		{
-			return (
-				<Item item={item}/>
-		    );
-		});
+  render() {
+    const itemsToShow = this.state.items.map((item) =>
+    {
+      return (
+        <Item item={item}/>
+        );
+    });
 
-		return (
-			<ul>
-				{itemsToShow}
-			</ul>
-		);
-	}
+    return (
+      <ul>
+        {itemsToShow}
+      </ul>
+    );
+  }
 }
 
 class Item extends Component {
-	render() {
-		return (
-			<li>
-				{this.props.item.content}
-	        </li>
-		)
-	}
+  render() {
+    return (
+      <li>
+        {this.props.item.content}
+          </li>
+    )
+  }
 }
 
 export default App;
