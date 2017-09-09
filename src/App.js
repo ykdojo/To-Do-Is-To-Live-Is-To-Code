438a4849742963c7fd916477d557890bd9627d44
yksugi <yksu4p@gmail.com>
2017-09-09 16:13:27 -0700

    handle checkbox change correctly

--- src/App.js
+++ src/App.js
@@ -5,20 +5,31 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      items: [{content: 'TODO Item 1', status: 'active'}, {id: 1, content: 'TODO Item 2', status: 'complete'}],
+      items: [{content: 'TODO Item 1', status: 'active'},
+              {content: 'TODO Item 2', status: 'complete'}],
     };
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
   }
 
-  handleCheckboxChange() {
-    // alert('hello');
+  handleCheckboxChange(event) {
+    const target = event.target;
+    const newStatus = target.checked ? 'complete' : 'active';
+    const itemID = target.getAttribute('itemid');
+    // Use Object.assign() instead of simply slice() to make a deep copy of
+    // |items|, which is an array of objects.
+    const items = this.state.items.map(o => Object.assign({}, o));
+    items[itemID].status = newStatus;
+    this.setState({
+        items: items
+    })
   }
 
   render() {
-    const itemsToShow = this.state.items.map((item) =>
+    const itemsToShow = this.state.items.map((item, index) =>
     {
       return (
-        <Item item={item} onCheckboxChange={this.handleCheckboxChange}/>
-        );
+        <Item key={index} itemID={index} item={item} onCheckboxChange={this.handleCheckboxChange}/>
+      );
     });
 
     return (
@@ -35,7 +46,7 @@ class Item extends Component {
 
     return (
       <li className={`todo-item ${this.props.item.status}`}>
-        <input type="checkbox" checked={isChecked}
+        <input type="checkbox" checked={isChecked} itemID={this.props.itemID}
         onChange={this.props.onCheckboxChange}
         /> {this.props.item.content}
       </li>
